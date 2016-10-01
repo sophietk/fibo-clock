642a656fbecf5ddca344ab4dabc77d8f497a44b8
sophietk <sophie.tk@gmail.com>
2016-10-01 13:32:14 +0200

    technical(npm): install npm project

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const chai = require('chai')
+const fiboClock = require('../lib/index.js')
+
+chai.should()
+
+describe('wip', function () {
+  it('should return true', function () {
+    true.should.be.true
+  })
+})
